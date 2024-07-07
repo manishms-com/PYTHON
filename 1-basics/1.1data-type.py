2dfa57495bd83ce69c7a0b4991ec2de08d680ffa
manishms-com <ms09735277@gmail.com>
2024-07-07 18:08:28 +0530

    basic  first commit

---
+++ 1-basics/1.1data-type.py
@@ -0,0 +1,15 @@
+a=111
+b=20
+
+print("Addition:",a+b)
+
+print("Subtraction:",a-b)
+
+print("Multiplition:",a*b)
+
+print("Division:",a/b)
+
+print("Modulus:",a%b)
+
+print("Integral division:",a//b)
+
